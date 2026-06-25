ff362e818c69c933ab655e0e581410c986602c79
xioalin <2099248663@qq.com>
2026-06-25 14:09:44 +0800

    模块化新闻分类列表

--- config/db_config.py
+++ config/db_config.py
@@ -41,3 +41,5 @@ async def get_db():
 
 
 
+
+
--- routers/news.py
+++ routers/news.py
@@ -2,7 +2,19 @@ from fastapi import APIRouter
 
 news_router = APIRouter(prefix= "/api/router/", tags=["news"])
 
+
+
+#接口实现流程
+#模块化路由-》参照API接口规范文档
+#定义数据库，表结构 ——〉参照数据库设计文档
+#定义crud函数，封装到crud表
+#路由调用crud方法
 @news_router.get('/categories')
-async def get_categories():
-    return {'mag': '类别获取成功'}
+async def get_categories(skip: int = 0, limit: int = 100):
+    return {
+        'code': 200,
+        'mag': '类别获取成功',
+        'data': '数据列表'
+    }
+
 
