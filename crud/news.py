d813bb74bb55946b322116518dfbd42955fd97eb
xioalin <2099248663@qq.com>
2026-06-25 19:21:35 +0800

    增加新闻详情查询方法

--- crud/news.py
+++ crud/news.py
@@ -4,6 +4,7 @@ from sqlalchemy.ext.asyncio import  AsyncSession
 from sqlalchemy import select
 from models import news
 
+
 #查新闻分页列表
 async def get_categories(db: AsyncSession, skip: int = 0, limit: int = 100):
     db_categories = select(news.Category).offset(skip).limit(limit)
@@ -11,5 +12,10 @@ async def get_categories(db: AsyncSession, skip: int = 0, limit: int = 100):
     return result.scalars().all()
 
 
+#查新闻详情
+async def get_news(db: AsyncSession, category_id: int, skip: int = 0, limit: int = 10):
+    db_news = select(news.News).where(news.News.category_id == category_id).offset(skip).limit(limit)
+    result = await  db.execute(db_news)
+    return result.scalars().all()
 
 
