71e817f57384b4958c958771e31ea48ba3562526
xioalin <2099248663@qq.com>
2026-06-25 18:33:10 +0800

    新闻列表模块化路由

--- routers/news.py
+++ routers/news.py
@@ -1,5 +1,9 @@
-from fastapi import APIRouter, Depends
+
+from fastapi import APIRouter, Depends, Query
+
+
 from sqlalchemy.ext.asyncio import AsyncSession
+
 from config import db_config
 from crud import news
 
@@ -12,7 +16,7 @@ news_router = APIRouter(prefix="/api/news", tags=["news"])
 #定义crud函数，封装到crud表
 #路由调用crud方法
 @news_router.get('/categories')
-async def get_new_list(skip: int = 0, limit: int = 100, db: AsyncSession = Depends(db_config.get_db)):
+async def get_new_categories(skip: int = 0, limit: int = 100, db: AsyncSession = Depends(db_config.get_db)):
     #调用crud方法
     x = await news.get_categories(db, skip, limit)
     return {
@@ -20,3 +24,21 @@ async def get_new_list(skip: int = 0, limit: int = 100, db: AsyncSession = Depen
         'mag': '类别获取成功',
         'data': x
     }
+
+@news_router.get("/list")
+async def get_new_list(
+        category_id: int = Query(..., alias="categoryId"),
+        page: int = 1,
+        page_size: int = Query(default=10, alias="pageSize",le= 100),
+):
+    return {
+        'code': 200,
+        'msg': "success",
+        "data": {
+            "list": '列表',
+            "total": "总量",
+            "hasMore": "是否更多"
+                }
+    }
+
+
