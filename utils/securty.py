70dcaf282de7316dca58e9ea286707fc805f98e1
xioalin <2099248663@qq.com>
2026-06-28 19:23:06 +0800

    添加注释

--- utils/securty.py
+++ utils/securty.py
@@ -3,7 +3,7 @@ from passlib.context import CryptContext
 #创建密码上下文
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-
+#用hash方法吧用户输入的明文转加密
 async def get_password_hash(password: str) -> str:
     return pwd_context.hash(password)
 
