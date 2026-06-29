8a345bb65c6b044249331e99a014a36eb7e03742
xioalin <2099248663@qq.com>
2026-06-29 16:21:13 +0800

    新增复写模型

--- schemas/users.py
+++ schemas/users.py
@@ -1,5 +1,31 @@
-from pydantic import BaseModel
+from typing import Optional
+
+from pydantic import BaseModel, Field, ConfigDict
+
 
 class UserRequest(BaseModel):
     username: str
     password: str
+
+class Userinfo(BaseModel):
+    nickname: Optional[str] = Field(default=None, max_length=150, description="昵称")
+    avatar: Optional[str] = Field(None, max_length=255, description="头像URL")
+    gender: Optional[str] = Field(None, max_length=10, description="性别")
+    bio: Optional[str] = Field(None, max_length=500, description="个人简介")
+
+class UserinfoResponse(Userinfo):
+    id : int
+    username : str
+
+    model_config = ConfigDict(
+        from_attributes=True
+    )
+
+
+class UserAuthResponse(BaseModel):
+    token: str
+    userinfo: Userinfo = Field(..., alias="userInfo")
+    #模型配置
+    model_config = ConfigDict(
+         populate_by_name=True,
+         from_attributes=True)
---
+++ utils/response.py
@@ -0,0 +1,13 @@
+#弄一个工具函数来封装响应类容
+from fastapi.encoders import jsonable_encoder
+from starlette.responses import JSONResponse
+
+
+def success_response(code, message : str = "success", data= None):
+    content =  {
+        "code": code,
+        "message": message,
+        "data": data
+    }
+    #把 content 变成“纯 JSON 数据”: fastapi, ORM, Pydantic对象 编程code, massage, data
+    return JSONResponse(content=jsonable_encoder(content))
