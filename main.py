1aca9a820d1d0607491657ce777cab2490c1e675
xioalin <2099248663@qq.com>
2026-06-29 22:09:09 +0800

    配置全局异常处理器

--- main.py
+++ main.py
@@ -2,7 +2,12 @@ from fastapi import FastAPI
 from routers import news, users
 #解决跨域问题
 from fastapi.middleware.cors import CORSMiddleware
+from utils.exception_response import register_exception_handlers
+
 app = FastAPI()
+
+#注册异常处理器
+register_exception_handlers(app)
 
 app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_credentials=True, allow_methods=["*"],
                    allow_headers=["*"])
